4c45d19d65e6f2cd9c2e2d78f22e114e697e8d4e
Barrett Mize <barrettdmize@gmail.com>
2016-04-14 10:34:07 -0700

    updating repo

--- main.js
+++ main.js
@@ -3,19 +3,69 @@
 
 
 //Below is the function for the slideshow
-$(function() {
-  var sectionOne = $('.sectionOne');
-  var backgrounds = ['url(assets/sunlightgirl.jpg)', 'url(assets/frontpagebackground.jpg)', 'url(assets/cupoffruit.jpg)', 'url(assets/yogastance.jpg)', 'url(assets/foodhealthyfruitsbreakfast.jpg)'];
-var current = 0;
+(function() {
 
-function nextBackground() {
-  sectionOne.css(
-   'background',
-    backgrounds[current = ++current % backgrounds.length]
- );
+	function Slideshow( element ) {
+		this.el = document.querySelector( element );
+		this.init();
+	}
 
- setTimeout(nextBackground, 5000);
- }
- setTimeout(nextBackground, 5000);
-   sectionOne.css('background', backgrounds[0]);
- });
+	Slideshow.prototype = {
+		init: function() {
+			this.wrapper = this.el.querySelector( ".slider-wrapper" );
+			this.slides = this.el.querySelectorAll( ".slide" );
+			this.previous = this.el.querySelector( ".slider-previous" );
+			this.next = this.el.querySelector( ".slider-next" );
+			this.index = 0;
+			this.total = this.slides.length;
+			this.timer = null;
+
+			this.action();
+			this.stopStart();
+		},
+		_slideTo: function( slide ) {
+			var currentSlide = this.slides[slide];
+			currentSlide.style.opacity = 1;
+
+			for( var i = 0; i < this.slides.length; i++ ) {
+				var slide = this.slides[i];
+				if( slide !== currentSlide ) {
+					slide.style.opacity = 0;
+				}
+			}
+		},
+		action: function() {
+			var self = this;
+			self.timer = setInterval(function() {
+				self.index++;
+				if( self.index == self.slides.length ) {
+					self.index = 0;
+				}
+				self._slideTo( self.index );
+
+			}, 3000);
+		},
+		stopStart: function() {
+			var self = this;
+			self.el.addEventListener( "mouseover", function() {
+				clearInterval( self.timer );
+				self.timer = null;
+
+			}, false);
+			self.el.addEventListener( "mouseout", function() {
+				self.action();
+
+			}, false);
+		}
+
+
+	};
+
+	document.addEventListener( "DOMContentLoaded", function() {
+
+		var slider = new Slideshow( "#main-slider" );
+
+	});
+
+
+})();
